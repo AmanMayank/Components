a001eff0aefc92875c01655df7a2c769591d541b
AmanMayank <aman.msingh87@gmail.com>
2023-05-31 02:28:37 +0530

    Added components

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,13 +1,16 @@
 import classNames from 'classnames';
 import useNavigation from '../hooks/use-navigation';
 
-function Link({to, children}) {
-  const { navigate } = useNavigation(); 
+function Link({to, children, className, activeClassName}) {
+  const { navigate, currentPath } = useNavigation(); 
   
-  const classes = classNames('text-blue-500')
+  const classes = classNames(
+   'text-blue-500',
+   className, 
+   currentPath === to && activeClassName)
 
   const handleClick = (event) => {
-    if(event.maetaKey || event.ctrlKey){
+    if(event.metaKey || event.ctrlKey){
         return;
     }
     event.preventDefault();
