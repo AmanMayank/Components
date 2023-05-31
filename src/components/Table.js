f337da856e11de963748fa046dfdce45a58341f2
AmanMayank <aman.msingh87@gmail.com>
2023-05-31 15:16:20 +0530

    basic table added

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,26 @@
-import React from 'react'
+function Table({data}) {
 
-function Table() {
+  const renderedTable =  data.map((row) => {
+        return <tr className="border-b" key={row.name}>
+            <td className="p-3">{row.name}</td>
+            <td className="p-3"><div className={`p-3 m-2 ${row.color}`}></div></td>
+            <td className="p-3">{row.score}</td>
+          </tr>
+    })
+ 
   return (
-    <div>Table</div>
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">
+          <th>Fruits</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+          {renderedTable}
+      </tbody>
+    </table>
   )
 }
 
