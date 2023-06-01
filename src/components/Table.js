a25b5294788252c4a2c12e51548add6092ae07d4
AmanMayank <aman.msingh87@gmail.com>
2023-06-02 01:46:19 +0530

    AddedSortingToTable

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,17 @@
+import { Fragment } from "react";
+
 function Table({ data, config }) {
   const renderedHeaders = config.map((column) => {
+    if (column.header) {
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
+    }
     return <th key={column.label}>{column.label}</th>;
   });
 
   const renderedRows = data.map((row) => {
     const renderedCells = config.map((column) => {
       return (
-        <td className="p-2" key={config.label}>
+        <td key={column.label} className="p-2">
           {column.render(row)}
         </td>
       );
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,5 @@
-import Table from "../components/Table";
+// import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -9,17 +10,25 @@ function TablePage() {
   ];
 
   const config = [
-    { label: "Name", render: (fruit) => fruit.name },
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
+    },
     {
       label: "Color",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
     },
-    { label: "Score", render: (fruit) => fruit.score },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      sortValue: (fruit) => fruit.score,
+    },
   ];
 
   return (
     <div>
-      <Table data={data} config={config} />
+      <SortableTable data={data} config={config} />
     </div>
   );
 }
